9540e60c509e5af4cecf3f38108b7b58d0c7e4fe
choky603 <arima603@gmail.com>
2025-02-23 17:50:49 +0900

    zod 밸리데이션 테스트

--- frontend/src/features/todo/components/AddTodo.vue
+++ frontend/src/features/todo/components/AddTodo.vue
@@ -13,15 +13,28 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { z } from 'zod'
 import { useTodoStore } from '@/features/todo/store/useTodoStore'
 
 const title = ref('')
 const todoStore = useTodoStore()
 
+const titleScheme = z
+  .string()
+  .min(2, { message: '조금더 입력하세요' })
+  .max(50, { message: '너무 길어요' })
+
 const add = () => {
-  if (title.value.trim()) {
+  try {
+    titleScheme.parse(title.value.trim())
     todoStore.addTodo(title.value.trim())
     title.value = ''
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      alert(error.errors.map((err) => err.message).join('\n'))
+    } else {
+      console.log(error)
+    }
   }
 }
 </script>
